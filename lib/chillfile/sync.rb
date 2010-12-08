0332657f48ebd92bd2da7f160c8ccef56ddb0f00
dpree <whiterabbit.init@gmail.com>
2010-12-08 15:45:00 +0100

    fixed specs

--- lib/chillfile/sync.rb
+++ lib/chillfile/sync.rb
@@ -4,47 +4,51 @@ module Chillfile
       def process!(comparator, progressbar = nil)
         @progressbar = progressbar
         
-        # created files
-        process_list(comparator.created, "Created") do |checksum, path|
+        # deleted
+        process_list(comparator.deleted, "Delete") do |checksum, path|
           for_doc_with(checksum) do |doc|
-            if doc
-              update_doc_add_path(doc, path)
+            if doc.paths.size > 1
+              update_doc_paths(doc, [path])
             else
-              create_doc(checksum, path)
+              delete_doc(doc)
             end
           end
         end
         
         # moved files
-        process_list(comparator.moved, "Moved") do |checksum, paths|
+        process_list(comparator.moved, "Move") do |checksum, paths|
           for_doc_with(checksum) do |doc|
             update_doc_paths(doc, paths[:old_paths], paths[:new_paths])
           end
         end
       
         # modified files
-        process_list(comparator.modified, "Modified") do |path, checksums|
+        process_list(comparator.modified, "Modify") do |path, checksums|
           for_doc_with(checksums[:old_checksum]) do |doc|
-          
+            
             # split it up if there is more than one path
             if doc.paths.size > 1
-              base_doc = update_doc_paths(doc, [path]), 
-              forked_doc = create_doc(checksums[:new_checksum], path)
-            
-              [base_doc, forked_doc]
+              
+              # remove the old path from the list
+              base_doc = update_doc_paths(doc, [path])
+              
+              # new forked doc
+              fork_doc = create_doc(checksums[:new_checksum], path)
+              
+              [base_doc, fork_doc]
             else
-              update_doc_attachment(doc, checksums[:new_checksum])  
+              update_doc_attachment(doc, checksums[:new_checksum])
             end          
           end
         end
-      
-        # deleted
-        process_list(comparator.deleted, "Deleted") do |checksum, path|
+        
+        # created files
+        process_list(comparator.created, "Create") do |checksum, path|
           for_doc_with(checksum) do |doc|
-            if doc.paths.size > 1
-              update_doc_paths(doc, [path])
+            if doc
+              update_doc_add_path(doc, path)
             else
-              delete_doc(doc)
+              create_doc(checksum, path)
             end
           end
         end
@@ -67,12 +71,10 @@ module Chillfile
     
       # COPIED
       def update_doc_add_path(doc, path)
-        doc.paths << path
-        
-        doc
+        update_doc_paths(doc, [], [path])
       end
     
-      # MOVED
+      # MOVED/COPIED
       def update_doc_paths(doc, del_paths = [], add_paths = [])
         # del old paths
         del_paths.each do |path|
@@ -106,6 +108,8 @@ module Chillfile
       
       # wrapper outer
       def process_list(list, title, &block)
+        return true if list.empty?
+        
         list_iterator = lambda do |notifier|
           list.each do |args|
             block.call(*args)
